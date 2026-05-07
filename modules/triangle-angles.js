f3e14243769aaa3d39a36d86e537f887e8b9ef0f
Theklan <34723367+Theklan@users.noreply.github.com>
2026-05-07 10:10:30 +0200

    Remove hint text span from footer
    Removed the creation of a span element for hint text in the footer.

--- modules/triangle-angles.js
+++ modules/triangle-angles.js
@@ -348,11 +348,6 @@ footer.className = 'hw-footer';
 var fImg = document.createElement('img');
 fImg.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Hiruwiki.svg/32px-Hiruwiki.svg.png';
 fImg.alt = 'Hiruwiki'; fImg.width = 22; fImg.height = 22;
-var fText = document.createElement('span');
-fText.innerHTML = t('hint').replace(
-    /(visualización interactiva|visualisation interactive|interactieve visualisatie|léirshamhlú idirghníomhach|visualització interactiva|대화형 시각화|bistaratzaile interaktibo|interactive visualization)/i,
-    '<strong>$1</strong>'
-);
 footer.appendChild(fImg);
 footer.appendChild(fText);
 container.appendChild(footer);
